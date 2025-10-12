77205d65f2ecb733a95efe01f5067f63934ba3a1
ToniokaSan <potoyaleman991@gmail.com>
2025-10-12 17:11:19 -0600

    Primera version

---
+++ src/main/java/com/mycompany/proyectomvpg7/ProyectoMVPG7.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectomvpg7;
+
+/**
+ *
+ * @author potoy
+ */
+public class ProyectoMVPG7 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
